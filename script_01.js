4e609b6df10a143c6246e93eec0b155ef31ac71f
JGRO182 <j-p.grosse-kohlbrecher@web.de>
2021-11-18 11:37:13 +0100

    Test
    Signed-off-by: JGRO182 <j-p.grosse-kohlbrecher@web.de>

--- script_01.js
+++ script_01.js
@@ -1,2 +1,35 @@
 /***** Variablen 01 *******/
 
+// Zeilen-Kommentar
+
+/**
+ * Block-Kommentar
+ */
+
+
+/********Deklaration & Wertzuweisung *******/
+
+// let firstName = "Max";
+// let lastName;
+// lastName = "Mustermann";
+// let age = 32;
+
+// alter = 5;
+
+
+// console.log("Hello World!");
+// console.log(firstName);
+// console.log("alter: "+alter);
+// alter += 1;
+// console.log("alter:",alter);
+
+
+let firstName, lastName;
+firstName = prompt("Bitte Vornamen eingeben");
+lastName = prompt("Bitte Nachnamen eingeben");
+console.log(firstName+" "+lastName);
+
+firstName = prompt("Bitte Vorname eingeben");
+console.log(firstName+" "+lastName);
+
+
